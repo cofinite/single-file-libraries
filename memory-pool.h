fb91f9859ca0163563a142e3dcf6d75227586373
Cofinite <>
2018-10-24 13:26:43 +0300

    Small fixes.

--- memory-pool.h
+++ memory-pool.h
@@ -19,25 +19,18 @@ How to use:
 
         MemPool(T) pool = mpInit(&pool);
         
-    Then, you may call `mpGrowPool` to increase the initial capacity of the pool
-    before allocating any objects, or simply begin calling `mpAlloc` to start
-    allocating objects:
+    Then, you may call `mpGrowPool` to increase the initial capacity of the
+    pool, or simply begin calling `mpAlloc` to start allocating objects:
         
         mpGrowPool(&pool, 100); // optional
         size_t handle = mpAlloc(&pool);
         
-    `mpAlloc` returns `size_t` handles into the memory pool, which are valid as
-    long as they are not freed via `mpFree` or the pool destroyed via
-    `mpFreePool`.
-    
+    `mpAlloc` returns a handle into the memory pool, which is valid as long as
+    it is not freed via `mpFree` or the entire pool freed via `mpFreePool`.
     To access an object via its handle, use the `mpAt` macro:
         
-        T foo;
-        foo.x = 1;
-        foo.y = 2;
-        foo.z = 3;
-        
-        mpAt(&pool, handle) = foo;
+        mpAt(&pool, handle).foo = 1;
+        mpAt(&pool, handle).bar = mpAt(&pool, handle).foo + 1;
         
     Once you are done using an object, call `mpFree` with the object's handle:
         
@@ -45,7 +38,6 @@ How to use:
         
     This will not free any memory, but it will make the object available for
     `mpAlloc` to allocate again.
-    
     Once you are done using the memory pool, call `mpFreePool` to release all
     memory associated with it:
         
@@ -55,22 +47,20 @@ How to use:
     The same pool may be used again via `mpGrowPool` and `mpAlloc` to allocate
     new handles.
 
-Errors:
+Notes:
+    
+    `mpAt` does not perform bounds checking, nor does it make sure that the
+    handle is valid. Call it only with valid handles.
     
     `mpGrowPool` will return 0 on success and -1 in an out-of-memory situation.
     
     `mpAlloc` will return a valid handle on success and `MP_INVALID_HANDLE` in
     an out-of-memory situation.
-
-Notes:
     
     If you need to refer to the same type of `MemPool` several times, you should
     `typedef` it first:
         
-        typedef MemPool(int) MemPoolInt;
-        
-    If you need to know the amount of memory used by the pool, call `mpCapacity`
-    to get the number of currently allocatable objects.
+        typedef MemPool(int) IntPool;
 
 
 LICENSE
@@ -129,9 +119,6 @@ void    mpFreeEx        (MemPoolInfo* this, size_t handle);
 
 #define _MP_GROWTH_FACTOR_NUMERATOR     3
 #define _MP_GROWTH_FACTOR_DENOMINATOR   2
-#define _MP_DEFAULT_INITIAL_CAPACITY    16
-
-typedef MemPool(size_t) _MpDummy;
 
 static int _mpResize(MemPoolInfo* this, size_t capacity)
 {
@@ -144,8 +131,17 @@ static int _mpResize(MemPoolInfo* this, size_t capacity)
     return 0;
 }
 
+static size_t* _mpNext(MemPoolInfo* this, size_t handle)
+{
+    return (size_t*)((char*)this->pBlocks + handle * this->blockSize);
+}
+
 int mpGrowPoolEx(MemPoolInfo* this, size_t num)
 {
+    size_t newCapacity = this->capacity + num;
+    if (newCapacity < this->capacity) {
+        return -1;
+    }
     return _mpResize(this, this->capacity + num);
 }
 
@@ -153,8 +149,8 @@ void mpFreePoolEx(MemPoolInfo* this)
 {
     if (this->pBlocks != NULL) {
         free(this->pBlocks);
+        this->pBlocks = NULL;
     }
-    this->pBlocks = NULL;
     this->capacity = 0;
     this->hFreeArray = 0;
     this->hFreeList = MP_INVALID_HANDLE;
@@ -164,13 +160,18 @@ size_t mpAllocEx(MemPoolInfo* this)
 {
     size_t handle = this->hFreeList;
     if (handle != MP_INVALID_HANDLE) {
-        this->hFreeList = *(size_t*)((char*)this->pBlocks + handle * this->blockSize);
+        this->hFreeList = *_mpNext(this, handle);
         return handle;
     }
     if (this->hFreeArray >= this->capacity) {
-        size_t newCapacity = this->capacity * _MP_GROWTH_FACTOR_NUMERATOR / _MP_GROWTH_FACTOR_DENOMINATOR;
-        if (newCapacity == 0) {
-            newCapacity = _MP_DEFAULT_INITIAL_CAPACITY;
+        size_t newCapacity = this->capacity *
+            _MP_GROWTH_FACTOR_NUMERATOR /
+            _MP_GROWTH_FACTOR_DENOMINATOR;
+        if (newCapacity < this->capacity) {
+            return MP_INVALID_HANDLE;
+        }
+        if (newCapacity == this->capacity) {
+            newCapacity += 1;
         }
         if (_mpResize(this, newCapacity) != 0) {
             return MP_INVALID_HANDLE;
@@ -184,7 +185,7 @@ size_t mpAllocEx(MemPoolInfo* this)
 void mpFreeEx(MemPoolInfo* this, size_t handle)
 {
     if (handle != MP_INVALID_HANDLE) {
-        *(size_t*)((char*)this->pBlocks + handle * this->blockSize) = this->hFreeList;
+        *_mpNext(this, handle) = this->hFreeList;
         this->hFreeList = handle;
     }
 }
